f1198a3f5233763b14f94016e6bfef3f4080eefa
Kitami-Michito <reikomu_0428@yahoo.co.jp>
2023-04-20 00:49:58 +0900

    チュートリアル入れてソースコード入力

--- src/apifront/getRate.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getRateApi ={
-    async getFumenRate(fumenName: string ): Promise<number> {
-
-        const response = await axios.get("https://nvpfae6628.execute-api.us-east-2.amazonaws.com/dev/rategetemer");
-        return response.data.songRate;
-    }
-}
