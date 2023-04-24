9eac19f9d8265c0d8aed9b163767e26c56ee55d9
Kitami-Michito <reikomu_0428@yahoo.co.jp>
2023-04-25 02:18:53 +0900

    ビルドが通るよう不具合修正(usestateの削除)、なおビルドするとページが表示されない

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { store } from '@/redux/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Provider, useStore } from 'react-redux'
+import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 
+
 export default function App({ Component, pageProps }: AppProps) {
-  const store = useStore()
+  
   const persistor = persistStore(store)
 
   return (
