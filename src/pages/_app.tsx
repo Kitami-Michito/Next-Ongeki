c4d1bb8f22d064cdfe843520540ed81373aa52fe
Kitami-Michito <reikomu_0428@yahoo.co.jp>
2023-05-01 22:48:40 +0900

    不要だったredux-persistを削除、yranlockも併せて削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,14 @@ import { store } from '@/redux/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  
-  const persistor = persistStore(store)
 
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
+      <Component {...pageProps} />
     </Provider>
   )
 }
